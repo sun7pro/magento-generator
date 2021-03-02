099da6c9ef821f310c6c15c2ed6e63afa731213c
Tuan Pham <tuanpht.online@gmail.com>
2021-03-02 22:59:35 +0700

    Init module, composer

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # magento-generator
+
+Magento 2 module that provides a set of commands to generate magento module, plugin, observer, config, graphql,...
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Sun7Pro_Generator',
+    __DIR__
+);
